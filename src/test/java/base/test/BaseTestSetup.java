9bb4c750fcdcaded0e98f1e422e869d2d69cf29d
Lacuran <kornel.wrzesien@gmail.com>
2022-10-06 11:33:01 +0200

    6.10 added base test

---
+++ src/test/java/base/test/BaseTestSetup.java
@@ -0,0 +1,32 @@
+package base.test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import live.panda.testing.VerifyCostOfProductTest;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+import org.testng.log4testng.Logger;
+
+public class BaseTestSetup {
+
+    public final static Logger LOGGER = Logger.getLogger(BaseTestSetup.class);
+    final String URL = "http://live.techpanda.org/";
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverManager.chromedriver().create();
+
+        LOGGER.info("WebDriver Initialization");
+        driver.manage().window().maximize();
+
+        LOGGER.info("1. Go to http://live.techpanda.org/");
+        driver.get(URL);
+    }
+
+    @AfterMethod
+    public void cleanUp() {
+
+        LOGGER.info("Cleanup Driver");
+        driver.quit();
+    }
+}
--- src/test/java/live/panda/testing/VerifyCostOfProductTest.java
+++ src/test/java/live/panda/testing/VerifyCostOfProductTest.java
@@ -1,39 +1,37 @@
 package live.panda.testing;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import base.test.BaseTestSetup;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
-import org.testng.log4testng.Logger;
+
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
-public class VerifyCostOfProductTest {
-    final static Logger LOGGER = Logger.getLogger(VerifyCostOfProductTest.class);
+public class VerifyCostOfProductTest extends BaseTestSetup {
 
     @Test
-    public void VerifyCostOfProductInListPageAndDetailsPageTest(){
+    public void verifyCostOfProductInListPageAndDetailsPageTest(){
 
         //variables
-        final String URL = "http://live.techpanda.org/";
         By mobileXpath = By.xpath("//*[text()='Mobile']");
-        By listPageXperiaPrice = By.xpath("//*[@id='product-price-1']");
-        By sonyXperiaXpath = By.xpath("//*[@title='Sony Xperia']");
-        By sonyXperiaDetailInfoXpath = By.xpath("//*[@class='price-info']");
 
-        LOGGER.info("Initialization chromedriver");
-        WebDriver driver = WebDriverManager.chromedriver().create();
-        driver.manage().window().maximize();
+//        By listPageXperiaPrice = By.xpath("//*[@id='product-price-1']");
+        By listPageXperiaPrice = By.cssSelector("#product-price-1"); //css selector (to learn) # - is for the id
 
-        LOGGER.info("1. Go to http://live.techpanda.org/");
-        driver.get(URL);
+//        By sonyXperiaXpath = By.xpath("//*[@title='Sony Xperia']");
+        By sonyXperiaXpath = By.cssSelector("[title='Sony Xperia']"); //css selector - [tag name='looked tag'] - specific tag
+
+//        By sonyXperiaDetailInfoXpath = By.xpath("//*[@class='price-info']");
+        By sonyXperiaDetailInfoXpath = By.cssSelector(".price-info"); //css selector . - css class
+
 
         LOGGER.info("2. Click on the 'MOBILE' menu");
         driver.findElement(mobileXpath).click();
 
         LOGGER.info("3. In the list of all mobile, read the cost of Sony XPeria mobile. Note this value.");
         String xperiaCost = driver.findElement(listPageXperiaPrice).getText();
-
 
         LOGGER.info("4. Click on the Sony XPeria mobile");
         driver.findElement(sonyXperiaXpath).click();
@@ -42,10 +40,43 @@ public class VerifyCostOfProductTest {
         String xperiaCostInDetailPage = driver.findElement(sonyXperiaDetailInfoXpath).getText();
 
         LOGGER.info("6. Compare value in Step 3 and Step 5");
-        assertEquals(xperiaCost, xperiaCostInDetailPage);
+        assertEquals(xperiaCost, xperiaCostInDetailPage,"Compare value in Step 3 and Step 5");
+//        assertNotEquals(xperiaCost,xperiaCostInDetailPage,"This is equal");
 
-        LOGGER.info("Clean up driver");
-        driver.quit();
+    }
+
+    @Test
+    public void verifyItemInTheMobileListPageCanBeShortedByNameTest() {
+        //variables
+        String expectedTitle = "Home page";
+        By mobileXpath = By.xpath("//*[text()='Mobile']"); // no string but by element
+        String mobileExpectedTitle = "Mobile";
+        By selectByNameDropdownXpath = By.xpath("(//*[@title='Sort By'])[1]");
+
+
+
+        LOGGER.info("2.Verify the title of the page");
+        assertEquals(driver.getTitle(), expectedTitle);
+
+        LOGGER.info("3.Click on Mobile menu");
+        driver.findElement(mobileXpath).click();
+
+        LOGGER.info("4.Verify the tittle of the page");
+        assertEquals(driver.getTitle(), mobileExpectedTitle);
+
+        LOGGER.info("5.In the list of all mobile, select SORT BY dropdown as name");
+        Select selectByName = new Select(driver.findElement(selectByNameDropdownXpath));
+        selectByName.selectByVisibleText("Name");
+
+        LOGGER.info("6.Verify all product are sorted by name");
+
+        List<String> phoneList = driver.findElements(By.xpath("//*[@class='product-image']")).stream()
+                .map(ele -> ele.getAttribute("title")).toList();
+
+        assertEquals(phoneList, phoneList.stream().sorted().toList());
+
+
+
     }
 
 }
